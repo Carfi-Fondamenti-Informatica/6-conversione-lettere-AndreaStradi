c8fd68a7e04ce9271baec4879f4b0e4e8e751861
AndreaStradi <91668754+AndreaStradi@users.noreply.github.com>
2021-11-30 18:12:28 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,19 @@
+//
+// Created by andre on 30/11/2021.
+//
 
+#include "lib.h"
+
+char funzione (char let){
+    char res;
+    if ((let >= 'a' && let <= 'z') || (let>='A' && let<='Z')) {
+        if (let >= 'a' && let <= 'z'){
+            res= let - 32;
+            return res;
+        }else if(let>='A' && let<='Z'){
+            res=let + 32;
+            return res;
+        }
+    }else{
+        return '0';
+    }}
